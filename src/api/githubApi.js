37403b7c0f5ee2fa4cb9df714f1c6be13d077742
Irteja <irteja500@gmail.com>
2026-02-01 11:44:41 +0600

    complete the function for performing async operation for getting the repos

--- src/api/githubApi.js
+++ src/api/githubApi.js
@@ -1,3 +1,16 @@
-export const fetchRandomRepo = async (language) => {
-  // Implementation will go here
+const BASE_URL = "https://api.github.com";
+
+export const searchRepositories = async (language) => {
+  const query = `language:${language}`;
+
+  const url = `${BASE_URL}/search/repositories?q=${query}&sort=starts&order=desc&per_page=50`;
+
+  const response = await fetch(url);
+
+  if(!response.ok){
+    throw new Error("Failed to fetch data from github!");
+  }
+
+  const data = await response.json();
+  return data.items;
 };
